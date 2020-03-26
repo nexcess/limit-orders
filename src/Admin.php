be91154b185749a5368105a8203d0b0ca515308f
Steve Grunwell <sgrunwell@liquidweb.com>
2020-03-26 21:01:32 +0000

    Add a 'Settings' action link on the plugin screen

--- src/Admin.php
+++ src/Admin.php
@@ -27,8 +27,26 @@ class Admin {
 	 * Add the necessary hooks.
 	 */
 	public function init() {
+		$basename = plugin_basename( dirname( __DIR__ ) . '/limit-orders.php' );
+
 		add_filter( 'woocommerce_get_settings_pages', [ $this, 'register_settings_page' ] );
 		add_filter( 'admin_notices', [ $this, 'admin_notice' ] );
+		add_filter( 'plugin_action_links_' . $basename, [ $this, 'action_links' ] );
+	}
+
+	/**
+	 * Inject a "Settings" link to the plugin's action links.
+	 *
+	 * @param array $actions A link of available actions.
+	 */
+	public function action_links( $actions ) {
+		array_unshift( $actions, sprintf(
+			'<a href="%s">%s</a>',
+			$this->get_settings_url(),
+			_x( 'Settings', 'plugin action link', 'limit-orders' )
+		) );
+
+		return $actions;
 	}
 
 	/**
@@ -58,7 +76,7 @@ class Admin {
 			echo wp_kses_post( sprintf(
 				/* Translators: %1$s is the settings page URL, %2$s is the reset date for order limiting. */
 				__( '<a href="%1$s">Based on your store\'s configuration</a>, new orders have been put on hold until %2$s.', 'limit-orders' ),
-				admin_url( 'admin.php?page=wc-settings&tab=limit-orders' ),
+				$this->get_settings_url(),
 				$this->limiter->get_next_interval_start()->format( get_option( 'date_format' ) )
 			) );
 		} else {
@@ -69,5 +87,14 @@ class Admin {
 			) );
 		}
 		echo '</p></div>';
+	}
+
+	/**
+	 * Retrieve a link to the plugin's settings page.
+	 *
+	 * @return string An absolute URL to the settings page.
+	 */
+	protected function get_settings_url() {
+		return admin_url( 'admin.php?page=wc-settings&tab=limit-orders' );
 	}
 }
